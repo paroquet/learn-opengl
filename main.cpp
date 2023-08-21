797cfaf3726d1d720f96be52e09b95d3991116fb
panqiang <panqiang@bilibili.com>
2023-08-21 19:02:30 +0800

    glm 依赖

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb_image.h>
-
-#include <shader.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
+
+#include <shader.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
