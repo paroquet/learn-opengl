bdf1ef1b7acf757fa9627f58358bb30853bc9831
panqiang <panqiang@bilibili.com>
2023-08-22 11:22:53 +0800

    变换

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void processInput(GLFWwindow *window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
