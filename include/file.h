fded34416f3b8f3ac1984ced5547c3e517653485
panqiang <panqiang@bilibili.com>
2023-08-24 14:27:29 +0800

    load first model

--- include/file.h
+++ include/file.h
@@ -9,3 +9,8 @@ std::string get_texture(const std::string &name) {
     std::string p = "/Users/pan/workspace_cxx/learn-opengl/resources/textures/" + name;
     return p;
 }
+
+std::string get_model(const std::string &name) {
+    std::string p = "/Users/pan/workspace_cxx/learn-opengl/resources/" + name;
+    return p;
+}
